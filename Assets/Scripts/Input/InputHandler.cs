85fee10060e6629c8e4f2f2266cef9f319b32b28
emiliano martel <alohacocki@gmail.com>
2025-05-26 19:54:51 -0300

    implement Input handler

---
+++ Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+namespace Input
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private InputSystem_Actions _inputActions;
+
+        public Action<Vector2> OnMove;
+
+        private void Awake()
+        {
+            _inputActions = new InputSystem_Actions();
+
+            _inputActions.Player.Move.performed += ctx =>
+            {
+                OnMove?.Invoke(ctx.ReadValue<Vector2>());
+            };
+
+            _inputActions.Player.Move.canceled += ctx =>
+            {
+                OnMove?.Invoke(Vector2.zero);
+            };
+        }
+
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+        }
+    }
+}
